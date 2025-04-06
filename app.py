b36206032be03aab9570f90cb1eb9ac897c7f9dc
KrishanMihiranga <xp3cto95@gmail.com>
2025-04-06 12:02:54 +0530

    feat: Logger

--- app.py
+++ app.py
@@ -1,27 +1,40 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template,request, redirect
 from pred_pipeline import preprocessing, vectorizer, get_prediction
+from logger import logging
 
 app = Flask(__name__)
 
-data = dict()
+logging.info('Flask server started')
 
+data = dict()
 reviews = []
 positive = 0
 negative = 0
+
 @app.route("/")
 def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
+
+    logging.info('========== Open home page ============')
+
     return render_template('index.html', data=data)
 
-@app.route("/", methods=['POST'])
-def create():
+@app.route("/", methods = ['post'])
+def my_post():
     review = request.form['review']
+    logging.info(f'review : {review}')
+
     preprocessed_review = preprocessing(review)
-    vectorized_preprocessed_review = vectorizer(preprocessed_review)
-    prediction = get_prediction(vectorized_preprocessed_review)
-    
+    logging.info(f'Preprocessed review : {preprocessed_review}')
+
+    vectorized_review = vectorizer(preprocessed_review)
+    logging.info(f'Vectorized review : {vectorized_review}')
+
+    prediction = get_prediction(vectorized_review)
+    logging.info(f'Prediction : {prediction}')
+
     if prediction == 'negative':
         global negative
         negative += 1
@@ -32,5 +45,5 @@ def create():
     reviews.insert(0, review)
     return redirect(request.url)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
